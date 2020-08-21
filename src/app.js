1d9080998ff7c2a9226ca5ddd90ad70737c62fb9
tonnyrogers <antoniel15975@gmail.com>
2020-08-21 10:49:17 -0300

    [fix] Cors 3

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ class App {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(cors({
-      origin: "*",
+      origin: 'https://agitated-swartz-1728c5.netlify.app/',
       methods: "GET,PUT,PATCH,POST,DELETE",
       optionsSuccessStatus: 200,
     }));
